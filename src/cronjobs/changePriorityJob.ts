7aa3056888805c3db85a022b2fbeb8e158fb1fd9
sparshmahajan <sparshmahajan3@gmail.com>
2024-02-01 19:58:57 +0530

    fix in change priority job

--- src/cronjobs/changePriorityJob.ts
+++ src/cronjobs/changePriorityJob.ts
@@ -1,10 +1,14 @@
 import { Task, TaskDoc } from "../models/taskSchema";
 import { taskPriorityCalculation } from "../common/taskPriorityCalculation";
+import { TaskStatusEnums } from "../enums/taskStatusEnum";
 
 export const changePriorityJob = async () => {
   const tasks = await Task.find({
     deleted_at: {
       $exists: false,
+    },
+    status: {
+      $ne: TaskStatusEnums.DONE,
     },
   });
 
